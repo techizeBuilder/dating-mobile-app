3a3b68d071d5613a60af93a221d962f3db83e7bd
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-09-11 19:18:43 +0530

    Update API URLs: uncomment production URLs and comment out local server URLs for clarity and proper environment configuration.

--- app/apiUrl.ts
+++ app/apiUrl.ts
@@ -1,10 +1,10 @@
 // Utkarsh's Server
-// export const API_BASE_URL = "https://heart2get-backend.onrender.com/api";
-// export const SOCKET_BASE_URL = "https://heart2get-backend.onrender.com";
+export const API_BASE_URL = "https://heart2get-backend.onrender.com/api";
+export const SOCKET_BASE_URL = "https://heart2get-backend.onrender.com";
 
 // Local server
-export const API_BASE_URL = "http://192.168.29.190:3000/api";
-export const SOCKET_BASE_URL = "http://192.168.29.190:3000";
+// export const API_BASE_URL = "http://192.168.29.190:3000/api";
+// export const SOCKET_BASE_URL = "http://192.168.29.190:3000";
 
 // //Farhan server
 // export const API_BASE_URL = "https://heart2get-server.onrender.com/api";
